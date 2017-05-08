25b80f8ca639a19794aa41cff25d4aac3fc0fd5e
LVMA <LVMA@LVMAs-iMac.local>
2017-05-08 11:11:35 +0100

    initial template data for about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { TemplateComponent } from '../shared/template.component';
+import { Observable } from 'rxjs/Observable';
+
 import { MotionService } from '../shared/motion.service';
 import { CachedHttpService } from '../shared/cached-http.service';
+import { TemplateContent } from '../shared/template-content';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
+  providers: [CachedHttpService]
 })
 export class AboutComponent implements OnInit {
 
-
+  aboutTemplate: Observable<TemplateContent>;
 
   constructor(
     private motionService: MotionService,
@@ -21,6 +24,9 @@ export class AboutComponent implements OnInit {
 
     const aboutPos = { home: [-.5, -.5], framer: [3, 5, '-90deg' ]};
     this.motionService.updatePosition(aboutPos);
+
+    const url = '../assets/about-me.json';
+    this.aboutTemplate = this.cachedHttpService.getFrom(url).data;
 
   }
 
