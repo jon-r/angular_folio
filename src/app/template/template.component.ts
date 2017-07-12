f13404c25a523c7a65ebeedffcf971e6e7dba010
LVMA <LVMA@LVMAs-iMac.local>
2017-07-12 17:08:24 +0100

    preparing for the loading content onscroll

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { useAnimation, transition, trigger, query } from '@angular/animations';
@@ -27,3 +27,24 @@ export class TemplateComponent implements OnInit {
   ngOnInit() {}
 
 };
+
+
+/* todo lazy load triggers:
+scroll event in app.component
+- note to target 'routesContainer' (is already linked in app component)
+or (scroll)="doShenanegans"
+
+trigger the route comms service which THROTTLES it
+- https://stackoverflow.com/questions/32051273/angular2-and-debounce
+- https://angular.io/api/core/NgZone#runOutsideAngular
+
+send this throttled info the template component, which matches scroll position to the positions of each element?
+- perhas something like
+#elTag  ngIf="scrollPos > elTag"
+- need to account for multiple ids??
+
+some pointers:
+https://stackoverflow.com/questions/40819739/angular-2-template-reference-variable-with-ngfor
+https://angular.io/guide/template-syntax#ref-vars
+
+*/
